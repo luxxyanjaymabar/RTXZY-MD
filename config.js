6298f4d3ae6c43ceadac31d1fe09b10c93c02867
luxxyanjaymabar <luxxyxyzvfx@gmail.com>
2022-09-10 17:32:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,23 +3,23 @@
 //Dalam file _menunya.js
 
 //『 Utama 』 //
-global.owner = ['6282xxxxxx']  // isi jika perlu
-global.mods = ['6282xxxxxx']   // isi jika perlu
-global.prems = ['6282xxxxxx']  // isi jika perlu
+global.owner = ['6288210072756']  // isi jika perlu
+global.mods = ['6288210072756']   // isi jika perlu
+global.prems = ['6288210072756']  // isi jika perlu
 
 //Isi dengan nama dan nomor lu//
-global.nameowner = '⫹⫺ OWNER' // isi nama kalian bebas
-global.numberowner = '6282xxxxxx' // isi nomor kalian dengan awalan seperti di contoh 
-global.instagram = 'https://instagram.com/prm2.0' // isi Instagram kalian harus link
-global.github = 'https://github.com/BOTCAHX' // gausah di isi juga gpp bro
-global.dana = '6282xxxxxx' // isi dengan nomor dana yg kamu punya 
-global.pulsa = '6282xxxxxx' // isi dengan nomor pulsa hp kalian
-global.gopay = ''
+global.nameowner = '⫹⫺ LuxxyVFX' // isi nama kalian bebas
+global.numberowner = '6288210072756' // isi nomor kalian dengan awalan seperti di contoh 
+global.instagram = 'https://instagram.com/luxxyxyz' // isi Instagram kalian harus link
+global.github = 'https://github.com/luxxyanjaymabar' // gausah di isi juga gpp bro
+global.dana = '6288210072756' // isi dengan nomor dana yg kamu punya 
+global.pulsa = '6288210072756' // isi dengan nomor pulsa hp kalian
+global.gopay = '6288210072756'
 
 //Name Bot & Grouplink//
-global.namebot = 'R-txzyMd'
-global.gc = 'https://chat.whatsapp.com/JEDQfzMGZan4HA98wtxsNL'
-global.web = 'https://myblogxyz.netlify.app' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'LuccyBot V2'
+global.gc = 'https://chat.whatsapp.com/CIv0mTBUteK8zy7crxdOj7'
+global.web = 'https://linktr.ee/luxxyxyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //Tanpilan wm Bot ( Sebagian Tidak di fungsi kan)//
@@ -27,10 +27,10 @@ global.fotonya1 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-stu
 global.fotonya2 = 'https://botcahx-rest-api.herokuapp.com/api/textpro/marvel-studios?text=List%20&text2=Menu' 
 global.lolkey = 'rey2k22' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '© R-TXZY-MD'
+global.wm = '© LuccyBot'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
+global.wm2 = '⫹⫺ LuccyBot'
+global.wm3 = '© LuccyBot'
 global.wm4 = '© 2021/2022'
 global.htki = '––––––『' 
 global.htka = '』––––––'
@@ -40,9 +40,9 @@ global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
-global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
+global.stiker_wait = '*⫹⫺ Stiker sedang dibuat sabar ya ngentot...*'
 global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.author = 'Bot WhatsApp\nLuccyBot'
 
 //Apikey Setting//
 global.APIs = { // API Prefix
